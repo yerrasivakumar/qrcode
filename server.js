308598ef32212f87fa44069844356987db7983a8
yerrasivakumar143 <yerrasiva123456@gmail.com>
2026-02-22 12:16:07 +0530

    2nd commit

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use("/api",auth);
 
 
 
-app.listen(3000, ()=>{
- console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port", process.env.PORT);
 });
